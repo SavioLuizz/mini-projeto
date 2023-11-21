86a093e1c47eeab13cd5d3a77685bdab7067a8db
SavioLuizz <savioluiz546@gmail.com>
2023-11-20 21:42:28 -0300

    class student

--- src/database/students.ts
+++ src/database/students.ts
@@ -1,19 +1,27 @@
 // Inicalizando o banco de dados da tabela estudantes;
 
-interface IStudent {
+class Student {
     id: number;
     nome: string;
     curso: string;
-    subjects: number[]
-    library: number[]
+    subjects: number[];
+    library: number[];
+
+    constructor(id: number, nome: string, curso: string, subjects: number[], library: number[]) {
+        this.id = id;
+        this.nome = nome;
+        this.curso = curso;
+        this.subjects = subjects;
+        this.library = library;
+    }
 }
 
 const InitStudents = async() => {
     const studentsRequest = await fetch("https://rmi6vdpsq8.execute-api.us-east-2.amazonaws.com/msAluno");
-    const json = await studentsRequest.json() as IStudent[]
+    const json = await studentsRequest.json() as Student[]
     const studentsParse = json.map(student => ({ ...student, subjects: [], library: [] }))
     students.push(...studentsParse)
 }
 
-export const students: IStudent[] = [];
+export const students: Student[] = [];
 InitStudents();
