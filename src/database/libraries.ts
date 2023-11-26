8b7fe5d30e92d3da9674df047289b3555c167c37
SavioLuizz <savioluiz546@gmail.com>
2023-11-26 16:25:09 -0300

    json in database

--- src/database/libraries.ts
+++ src/database/libraries.ts
@@ -1,10 +1,10 @@
 export const libraries = [
-    { id: 1, nome: "A" },
-    { id: 2, nome: "B" },
-    { id: 3, nome: "C" },
-    { id: 4, nome: "D" },
-    { id: 5, nome: "E" },
-    { id: 6, nome: "F" },
-    { id: 7, nome: "G" },
-    { id: 8, nome: "H" },
+    { "id": 1, "nome": "A" },
+    { "id": 2, "nome": "B" },
+    { "id": 3, "nome": "C" },
+    { "id": 4, "nome": "D" },
+    { "id": 5, "nome": "E" },
+    { "id": 6, "nome": "F" },
+    { "id": 7, "nome": "G" },
+    { "id": 8, "nome": "H" }
 ]
